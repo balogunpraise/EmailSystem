b3ac4626c4f829a64f5a25472cb1e352be254c7d
Praise <balogunpraise2@gmail.com>
2022-01-19 15:06:16 +0100

    My custom email service

---
+++ EmailService/Controllers/EmailController.cs
@@ -0,0 +1,39 @@
+﻿using EmailService.Models;
+using EmailService.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {
+        private readonly IMailService mailService;
+        public EmailController(IMailService mailService)
+        {
+            this.mailService = mailService;
+        }
+
+        [HttpPost("Send")]
+        public async Task<IActionResult> Send([FromForm] MailRequest request)
+        {
+            try
+            {
+                await mailService.SendEmailAsync(request);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+    }
+}
